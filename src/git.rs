1512a53576eb2aeb88d6866b0fb30c2b44e95ab7
matheussricardoo <matheusri1@hotmail.com>
2026-02-25 02:09:31 -0300

    feat(git): add is_cargo_lock_staged function and filter cargo.lock so it is not committed

--- src/git.rs
+++ src/git.rs
@@ -2,7 +2,9 @@ use anyhow::bail;
 use std::process::Command;
 
 pub fn get_staged_diff() -> anyhow::Result<String> {
-    let output = Command::new("git").args(["diff", "--staged"]).output()?;
+    let output = Command::new("git")
+        .args(["diff", "--staged", "--", ".", ":!Cargo.lock"])
+        .output()?;
     let diff = String::from_utf8(output.stdout)?;
     if !output.status.success() {
         let error_message = String::from_utf8_lossy(&output.stderr);
@@ -21,7 +23,11 @@ pub fn get_staged_diff_name_only() -> anyhow::Result<Vec<String>> {
         bail!("Git error: {}", error_message.trim());
     }
     let diff = String::from_utf8(output.stdout)?;
-    let split_archive_names_vec = diff.lines().map(|s| s.to_string()).collect();
+    let split_archive_names_vec = diff
+        .lines()
+        .filter(|line| !line.contains("Cargo.lock"))
+        .map(|s| s.to_string())
+        .collect();
 
     Ok(split_archive_names_vec)
 }
@@ -36,4 +42,15 @@ pub fn get_staged_diff_for_file(filename: &str) -> anyhow::Result<String> {
     }
     let diff = String::from_utf8(output.stdout)?;
     Ok(diff)
+}
+
+pub fn is_cargo_lock_staged() -> anyhow::Result<bool> {
+    let output = Command::new("git")
+        .args(["diff", "--staged", "--name-only", "--", "Cargo.lock"])
+        .output()?;
+    if !output.status.success() {
+        let error_message = String::from_utf8_lossy(&output.stderr);
+        bail!("Git error: {}", error_message.trim());
+    }
+    Ok(!output.stdout.is_empty())
 }
