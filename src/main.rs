de5f0710f2cf95d6dc14e33390bcd78f8a1fa3d8
matheussricardoo <matheusri1@hotmail.com>
2026-02-24 23:27:54 -0300

    feat(staging): add staging area checks for git diff and staged files

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
 use crate::llm::generate_commit_message;
+use clap::Parser;
 
 mod git;
 mod llm;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about = "AI-powered git commit generator")]
+struct Args {
+    #[arg(short, long)]
+    split: bool,
+
+    #[arg(short, long, default_value_t = 0.0)]
+    temp: f32,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let diff = git::get_staged_diff()?;
-    if diff.is_empty() {
-        println!("There are no files in the staging area. Did you forget to run ‘git add’?");
+    let args = Args::parse();
+
+    let files = git::get_staged_diff_name_only()?;
+    if files.is_empty() {
+        println!("No files in staging area...");
         return Ok(());
     }
-
-    let result = generate_commit_message(diff).await?;
-    println!("{}", result);
+    if args.split {
+        for filename in files {
+            let diff = git::get_staged_diff_for_file(&filename)?;
+            println!("--- File: {} ---", filename);
+            let message = generate_commit_message(diff, args.temp).await?;
+            println!("Suggestion: {}\n", message);
+        }
+    } else {
+        let diff = git::get_staged_diff()?;
+        let result = generate_commit_message(diff, args.temp).await?;
+        println!("{}", result);
+    }
     Ok(())
 }
